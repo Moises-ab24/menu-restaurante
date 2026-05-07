d5b46e5a52598ef8ddd80d8f39db49a6dcb41a09
Moises-ab24 <moi24ad@gmail.com>
2026-05-07 12:16:47 -0600

    Inicializar conexión con Firebase

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ ESTADO GLOBAL
 ======================================= */
 let db = null;
 let filterCat = 'all';
+
+/* ====================================
+FIREBASE - INICIALIZACIÓN
+======================================= */
+function initFirebase() {
+    try {
+        initFirebase.initializeApp(firebaseConfig);
+        db = firebase.database();
+
+        db.red('.info/connected').on('value', snap => {
+            setDbStatus(snap.val() === true ? 'connected' : 'disconnected');
+        });
+
+        startListening();
+    } catch (e) {
+        showToast('Error al conectar con Firebase: ' + e.message, 'error')
+        steDbStatus('error');
+    }
+}
