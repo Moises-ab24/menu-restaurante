0790858937f6527edf10ae3846a13eff2b730981
Moisés-ab24 <moi24ad@gmail.com>
2026-05-20 17:08:23 -0600

    Crear archivo de configuración de Firebase

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+/* ════════════════════════════════════════════════════════════
+   FIREBASE — CONFIGURACIÓN DEL PROYECTO
+   ════════════════════════════════════════════════════════════ */
+const firebaseConfig = {
+  apiKey: "AIzaSyAh1TH2gQSzrl1izmFsi8rnw7nZPmrKdjw",
+  authDomain: "menu-restaurante-8a7f5.firebaseapp.com",
+  databaseURL: "https://menu-restaurante-8a7f5-default-rtdb.firebaseio.com",
+  projectId: "menu-restaurante-8a7f5",
+  storageBucket: "menu-restaurante-8a7f5.firebasestorage.app",
+  messagingSenderId: "893980282257",
+  appId: "1:893980282257:web:672c3ce9e41ae45147de30"
+};
